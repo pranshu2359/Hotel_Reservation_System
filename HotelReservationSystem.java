1a57b34a6d13080eee511a3b02a1d26afaf91eb3
Pranshu Shriwastav <194409211+pranshu2359@users.noreply.github.com>
2026-01-04 23:02:57 +0530

    Update HotelReservationSystem.java

--- HotelReservationSystem.java
+++ HotelReservationSystem.java
@@ -5,7 +5,7 @@ public class HotelReservationSystem {
 
     private static final String url = "jdbc:mysql://localhost:3306/hotel_db";
     private static final String username = "root";
-    private static final String password = "235900";
+    private static final String password = "000000";
 
     public static void main(String[] args) {
         try {
@@ -189,4 +189,5 @@ public class HotelReservationSystem {
         }
         System.out.println("Thank you for using Hotel Reservation System!");
     }
+
 }
